8c328ac5ab5e05eba6f8249ab8cac820f6732700
AndreySmogunov <a.batman2003@yandex.ru>
2025-02-25 18:01:58 +0300

    Add files via upload

---
+++ src/constants/TaskStatus.java
@@ -0,0 +1,7 @@
+package constants;
+
+public enum TaskStatus {
+    NEW,
+    IN_PROGRESS,
+    DONE
+}
---
+++ src/tasks/Subtask.java
@@ -0,0 +1,27 @@
+package tasks;
+
+import constants.TaskStatus;
+
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String name, String description, int id, int epicId) {
+        super(name, description, id);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", id=" + getId() +
+                ", status=" + getStatus() +
+                ", epicId=" + epicId +
+                '}';
+    }
+}
