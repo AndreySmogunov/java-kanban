c63544a3b44b43c12c5eeb209455a33b5b3a98dd
AndreySmogunov <a.batman2003@yandex.ru>
2025-02-25 15:46:10 +0300

    Add files via upload

---
+++ src/Task.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+class Task {
+    private String name;
+    private String description;
+    private int id;
+    private TaskStatus status;
+
+    public Task(String name, String description, int id) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.status = TaskStatus.NEW;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
