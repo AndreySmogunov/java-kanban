f3b14e8a9f60d7cf6731718c778e1a7b78fd13e4
AndreySmogunov <a.batman2003@yandex.ru>
2025-03-10 15:40:37 +0300

    Add files via upload

---
+++ src/logic/HistoryManager.java
@@ -0,0 +1,10 @@
+package logic;
+
+import models.Task;
+
+import java.util.List;
+
+public interface HistoryManager {
+    void add(Task task);
+    List<Task> getHistory();
+}
---
+++ src/models/EpicTest.java
@@ -0,0 +1,22 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpicTest {
+
+    @Test
+    void createEpic() {
+        Epic epic = new Epic("Test Epic", "Test Description");
+        assertEquals("Test Epic", epic.getName(), "Epic name should be set correctly.");
+        assertEquals("Test Description", epic.getDescription(), "Epic description should be set correctly.");
+    }
+
+    @Test
+    void addSubtaskId() {
+        Epic epic = new Epic("Test Epic", "Test Description");
+        epic.addSubtaskId(1);
+        assertEquals(1, epic.getSubtaskIds().size(), "Subtask ID should be added to the epic.");
+    }
+}
