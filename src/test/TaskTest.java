2edcd1f3843c564f8fcdb6dd404322a6a6b29ebb
AndreySmogunov <a.batman2003@yandex.ru>
2025-03-31 18:44:27 +0300

    Add files via upload

---
+++ src/test/TaskTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void testExample() {
+        assertEquals(1, 1); // A very basic test
+    }
+}
