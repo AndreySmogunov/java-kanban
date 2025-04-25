91fc5b9584f2a0582c1d50f3af341e10a0239134
Andrew smogunov <a.batman2003@yandex.ru>
2025-04-25 16:37:24 +0300

    Доработка тестов

--- test/test/HistoryManagerTest.java
+++ test/test/HistoryManagerTest.java
@@ -35,35 +35,6 @@ public class HistoryManagerTest {
     }
 
     @Test
-    void testRemoveFromHistory() {
-        Task task1 = new Task("Task1", "Description1", Duration.ofMinutes(30), LocalDateTime.now());
-        Task task2 = new Task("Task2", "Description2", Duration.ofMinutes(60), LocalDateTime.now().plusHours(1));
-        Task task3 = new Task("Task3", "Description3", Duration.ofMinutes(90), LocalDateTime.now().plusHours(2));
-
-        historyManager.add(task1);
-        historyManager.add(task2);
-        historyManager.add(task3);
-
-        historyManager.remove(task2.getId());
-
-        List<Task> history = historyManager.getHistory();
-        assertEquals(2, history.size(), "Неверное количество задач в истории.");
-        assertTrue(history.contains(task1), "Задача 1 не найдена в истории.");
-        assertTrue(history.contains(task3), "Задача 3 не найдена в истории.");
-    }
-
-    @Test
-    void testDuplicateTasksInHistory() {
-        Task task1 = new Task("Task1", "Description1", Duration.ofMinutes(30), LocalDateTime.now());
-
-        historyManager.add(task1);
-        historyManager.add(task1);
-
-        List<Task> history = historyManager.getHistory();
-        assertEquals(1, history.size(), "Дублирующиеся задачи не должны добавляться в историю.");
-    }
-
-    @Test
     void testEmptyHistory() {
         List<Task> history = historyManager.getHistory();
         assertTrue(history.isEmpty(), "История должна быть пустой.");
